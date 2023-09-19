40b10f6a0357e03f5759c2bd7119c1e49915ddd2
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2023-09-19 04:07:02 +0200

    feat: implement a network call

---
+++ AppetizersApp/APError.swift
@@ -0,0 +1,16 @@
+//
+//  APError.swift
+//  AppetizersApp
+//
+//  Created by Kitso Namane on 18/09/23.
+//
+
+import Foundation
+
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
