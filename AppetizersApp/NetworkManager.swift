32f13fed038ff13bbfbb813ca8a18c2548036eb7
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2023-09-19 06:05:53 +0200

    feat: setup network image download

--- AppetizersApp/NetworkManager.swift
+++ AppetizersApp/NetworkManager.swift
@@ -5,10 +5,11 @@
 //  Created by Kitso Namane on 18/09/23.
 //
 
-import Foundation
+import UIKit
 
 final class NetworkManager {
     static let shared = NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerURL = baseURL + "appetizers"
@@ -23,7 +24,6 @@ final class NetworkManager {
         
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
             if let _ = error {
-                print(error)
                 completed(.failure(.unableToComplete))
                 return 
             }
@@ -47,6 +47,31 @@ final class NetworkManager {
             }
         }
     
+        task.resume()
+    }
+    
+    func downloadImage(fromUrlString urlString: String, completed: @escaping (UIImage?) -> Void) {
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else { 
+            completed(nil)
+            return 
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let data = data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
+        }
         task.resume()
     }
     
