da8271ab90b1a6972ace7c77183cc6ad0129ed07
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2023-09-19 05:28:26 +0200

    feat: implement errors and alerts

--- AppetizersApp/AppetizerListViewModel.swift
+++ AppetizersApp/AppetizerListViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 
 final class AppetizerListViewModel: ObservableObject {
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers() {
         NetworkManager.shared.getAppetizers { result in 
@@ -18,7 +19,16 @@ final class AppetizerListViewModel: ObservableObject {
                 case .success(let appetizers):
                     self.appetizers = appetizers
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    switch error {
+                    case .invalidURL:
+                        self.alertItem = AlertContext.invalidURL
+                    case .invalidResponse:
+                        self.alertItem = AlertContext.invalidResponse
+                    case .invalidData:
+                        self.alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        self.alertItem = AlertContext.unableToComplete
+                    }
                 }
             }
         }
--- AppetizersApp/NetworkManager.swift
+++ AppetizersApp/NetworkManager.swift
@@ -23,6 +23,7 @@ final class NetworkManager {
         
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
             if let _ = error {
+                print(error)
                 completed(.failure(.unableToComplete))
                 return 
             }
