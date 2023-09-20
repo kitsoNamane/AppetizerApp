71157bbd7b2b81aabafccb92e98d80e612eb3415
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2023-09-20 18:27:54 +0200

    feat: implement account tab form

--- AppetizersApp/Views/AccountView/AccountView.swift
+++ AppetizersApp/Views/AccountView/AccountView.swift
@@ -8,10 +8,43 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthDay = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
+    
     var body: some View {
         NavigationView {
-            Text("Appetizer Account View")
-            .navigationTitle("Accounts")
+            Form{
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                    
+                    TextField("Last Name", text: $lastName)
+                    
+                    TextField("Email Name", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                        
+                    DatePicker("Birthday", selection: $birthDay, displayedComponents: .date)
+                    
+                    Button {
+                        print("save changes")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+            }
+            .navigationTitle("🤣 Accounts")
         }
     }
 }
